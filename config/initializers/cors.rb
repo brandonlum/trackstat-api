06c593a7000defce333cc6168cfc9cccfe177c49
brandonlum <brandon.lum09@gmail.com>
2019-08-29 08:00:07 -0400

    Updating cors whitelisting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:3001', 'https://brandonlum.github.io/trackstat-client', 'https://brandonlum.github.io']
+    origins ['localhost:3001', 'https://brandonlum.github.io/trackstat', 'https://brandonlum.github.io']
 
     resource '*',
       headers: :any,
